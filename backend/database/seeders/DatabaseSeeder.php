0b716129451ba31bcce6de6528a7d80bd6386bb0
lawrencepenano <lawrence.olegario.penano@gmail.com>
2026-06-07 20:44:09 +0800

    feat: register CoA seeders in DatabaseSeeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -9,6 +9,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(AdminSeeder::class);
+        $this->call(AccountTypeSeeder::class);
+        $this->call(ChartOfAccountSeeder::class);
+        $this->call(ChartOfAccountSubtypeSeeder::class);
         $this->call(SubtypeSeeder::class);
         $this->call(DemoDataSeeder::class);
     }
