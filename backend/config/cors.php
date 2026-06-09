b9e174f3cb53fa148e6461afefe674cc9fcbaf43
lawrencepenano <lawrence.olegario.penano@gmail.com>
2026-06-10 00:29:59 +0800

    cors issue fix

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -11,7 +11,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => array_filter(array_map('trim', explode(',', env('FRONTEND_URL', 'http://localhost:3000')))),
 
     'allowed_origins_patterns' => [],
 
